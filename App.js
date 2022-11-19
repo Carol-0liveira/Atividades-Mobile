dca26e912cb682a62d1bc68b8cd58180c9319117
carolayne oliveira <carolayneoliveira150@gmail.com>
2022-11-19 12:42:25 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>CAROLAYNE MARIA DE OLIVEIRA</Text>
+      <Image source={require('./assets/Meri.jpg')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
